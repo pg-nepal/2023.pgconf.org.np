377c5d8dbb6c2de3141962b661fe04005c01f025
rho <rho.rhoit@gmail.com>
2024-12-19 11:46:15 +0545

    add pages

---
+++ srv/pages.py
@@ -0,0 +1,46 @@
+import flask
+
+from srv import app
+
+
+@app.route('/')
+def render_home():
+    return flask.render_template(
+        '/home.djhtml',
+    )
+
+
+@app.route('/<page>')
+def render_page(page):
+    return flask.render_template(
+        page + '.djhtml'
+    )
+
+
+@app.route('/speakers/all')
+def render_speaker_page():
+    from talks import keynotes,talks, workshops
+    return flask.render_template(
+        'speakers.djhtml',
+        keynotes  = keynotes,
+        talks     = talks,
+        workshops = workshops
+    )
+
+
+@app.route('/speaker/<slug>')
+def render_speaker_page_slug(slug):
+    from talks import keynotes,talks, workshops
+
+    talks_list = keynotes + talks + workshops
+    selected_talk = None
+
+    for talk in talks_list:
+        if talk['slug'] != slug:
+            continue
+        selected_talk = talk
+
+    return flask.render_template(
+        'speakers_indivisual.djhtml',
+        talk = selected_talk,
+    )
