83e8eb0dc01729ca5854da825727c78c9d95387f
rho <rho.rhoit@gmail.com>
2024-12-19 12:10:27 +0545

    auth file added

---
+++ srv/auth.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+
+import flask
+
+keys = {
+    'cm9vdDowCg==' : 'root',
+}
+
+
+def isValid(request):
+    """
+    returns {dict, user-obj} if authorized else None
+    """
+
+    auth = request.headers.get('Authorization')
+    if auth is None:
+        return False
+
+    uname = keys.get(auth)
+    if uname is not None:
+        return uname
+
+
+def respondInValid():
+    status = HTTPStatus.UNAUTHORIZED
+    response = flask.make_response(status.description, status.value)
+    response.headers['WWW-Authenticate'] = 'Basic'
+    return response
