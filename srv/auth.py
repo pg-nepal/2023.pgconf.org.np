8aaac690e3e320676613ec5f902fa398cf16ddcd
chimpKiller <shashankshrestha21@gmail.com>
2024-12-20 12:01:00 +0545

    decoding credentials and splitting it and checking ti

--- srv/auth.py
+++ srv/auth.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from dotenv import load_dotenv
+from base64 import b64decode
 
 import os
 import flask
@@ -9,7 +10,7 @@ load_dotenv()
 proposal_key = os.getenv('PROPOSAL_KEY')
 
 keys = {
-    'proposal_key' : 'root',
+    proposal_key : 'root',
 }
 
 
@@ -22,13 +23,31 @@ def isValid(request):
     if auth is None:
         return False
 
-    uname = keys.get(auth)
-    if uname is not None:
-        return uname
+    auth = auth.strip()
+    if not auth.lower().startswith('basic'):
+        return False
+
+    try:
+        encoded_credentials = auth[6:].strip()
+        decoded_credentials = b64decode(encoded_credentials).decode('utf-8')
+
+        # Splitting the decoded string into username and password
+        username, password = decoded_credentials.split(":", 1)
+
+        # Checking if the password matched the proposal key
+        if password == proposal_key and username == keys.get(password):
+            # Returning user object
+            return {"username": username, "role": "root"}
+    
+    except Exception as e:
+        return False
+    
+    return False
 
 
 def respondInValid():
     status = HTTPStatus.UNAUTHORIZED
     response = flask.make_response(status.description, status.value)
-    response.headers['WWW-Authenticate'] = 'Basic'
+    response.headers['WWW-Authenticate'] = 'Basic realm="Authorization Required"'
     return response
+
